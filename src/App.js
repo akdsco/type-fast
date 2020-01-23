e190b308320c0f6d31a8a23149f242c27e657267
Arkadiusz <arkadiusz@ostrowski.co>
2020-01-23 07:42:02 +0000

    add state hook to track textarea value

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [charCount, setCharCount] = useState('');
+
+  function handleCharChange(e) {
+    const {value} = e.target;
+    setCharCount(value)
+  }
+
   return (
     <div>
       <h1>Speed Typing Game</h1>
-      <textarea />
+      <textarea
+        onChange={handleCharChange}
+        value={charCount}
+      />
       <h4>Time Remaining</h4>
       <button>Start game</button>
       <h1>You typed x words</h1>
